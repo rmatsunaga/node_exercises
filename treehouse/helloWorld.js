7189278ce2f1636ac0f8475ff68226efe8d2e459
Rey <matsunagar97@gmail.com>
2018-12-21 16:25:24 +0900

    First Commit

---
+++ treehouse/helloWorld.js
@@ -0,0 +1,3 @@
+console.log("hello world");
+console.error("oops! Something went wrong");
+console.dir({"bboy": "Deathray", "Crew": "Tribe of the Handsome Warriors"});
---
+++ treehouse/userInfo.js
@@ -0,0 +1,2 @@
+// Problem: find user's badge count and javascript points in teamtreehouse.
+// Solution: use Node.js to connect to Treehouse's API to get profile and print out info.
