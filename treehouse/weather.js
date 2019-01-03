a36e7b74a94b7946b6d7ee48b266513b645152bb
Rey <matsunagar97@gmail.com>
2019-01-03 23:55:41 +0900

    added errors

--- treehouse/weather.js
+++ treehouse/weather.js
@@ -1,4 +1,5 @@
 const https = require('https')
+const http = require('http')
 const api = require('./api.json')
 
 
@@ -6,28 +7,43 @@ function printMessage(temp, name) {
     const message = "Current temperature in " + name + " is " + temp + " degrees, Celsius.";
     console.log(message);
 }
+const printError = (error) => {
+    console.error(error);
+}
 
 function get(query) {
-    const request = https.get('https://api.openweathermap.org/data/2.5/weather?q='+ query + '&units=metric&APPID=' + api.key, 
-    (res) => {
-        if(res.statusCode === 200) {
-            let body = ""
-            // console.log('you\'re getting something');
-            res.on('data', (data) => {
-                body += data.toString();
-               //  console.log(body);
-            });
-            res.on('end', () => {
-                // parse data
-                const profile = JSON.parse(body);
-                const temp = profile.main.temp;
-                const name = profile.name;
-                // print data
-                printMessage(temp, name);   
-            });
-        } else {
-            console.log("try something else");
-        }
-    });
+    try {
+        const readableQuery = query.replace('+', ' ');
+        const request = https.get('https://api.openweathermap.org/data/2.5/weather?q='+ query + '&units=metric&APPID=' + api.key, 
+        (res) => {
+            if(res.statusCode === 200) {
+                let body = ""
+
+                // console.log('you\'re getting something');
+                res.on('data', (data) => {
+                    body += data.toString();
+                   //  console.log(body);
+                });
+                res.on('end', () => {
+                    // parse data
+                    const profile = JSON.parse(body);
+                    if(profile.name) {
+                        // print data
+                        printMessage(profile.main.temp, profile.name); 
+                    } else {
+                        const queryError = new Error('The location for ' + readableQuery + 'was not found.');
+                        printError(queryError);
+                    }
+                      
+                });
+            } else {
+                const statusCodeError = new Error('There was an error getting the temperature for ' + readableQuery + '. ' + http.STATUS_CODES[res.statusCode]);
+                printError(statusCodeError);
+            }
+        });
+    } catch(error) {
+        printError(error);
+    }
+    
 }
 module.exports.get = get;
